c8b02740dbb50ab8906ea44556022b6b9ca749af
sunny7657 <ksvetlana466@gmail.com>
2024-06-18 23:52:01 +0200

    add global style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import type React from 'react';
+import './globals.css';
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
