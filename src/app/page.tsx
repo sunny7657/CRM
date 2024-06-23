ea75ec967b17f0b7e5416e327f76b4031ce17ab6
sunny7657 <ksvetlana466@gmail.com>
2024-06-23 22:33:53 +0200

    add clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <main>
       <h1 className="text-xl">Home page</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
