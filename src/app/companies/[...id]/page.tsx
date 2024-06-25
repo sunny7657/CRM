db418cbac24026fb329dfe760d4e17c88e12b34c
sunny7657 <ksvetlana466@gmail.com>
2024-06-25 23:40:01 +0200

    update folder name

--- src/app/companies/[...id]/page.tsx
+++ src/app/companies/[...id]/page.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from '../../components/Header';
 
 export interface PageProps {
-  params: { id: string };
+  params: { id: string[] };
 }
 
 export default function Page({ params }: PageProps) {
   return (
     <>
-      <Header>Companies ({params.id})</Header>
+      <Header>Companies ({String(params.id)})</Header>
+      <p>{new Date().toTimeString()}</p>
     </>
   );
 }
